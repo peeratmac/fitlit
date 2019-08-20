50b962383ba790d39afb1706f917251da70d35d7
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-08-20 16:11:20 -0600

    Add DOM manipulation for Hydration.  Fix the class and methods for Hydration.  Add new tests.
    Co-authored-by: Kirk Veitch<kirk.a.veitch@gmail.com>

--- src/Hydration.js
+++ src/Hydration.js
@@ -3,25 +3,23 @@ class Hydration {
     this.data = data;
   }
 
-  // get all the data from the specific user (all dates)
-  filterUser(idNumber) {
-    return this.data.filter(user => user.userID === idNumber);
+  getDailyWaterIntake(date) {
+    let dailyOunces = this.data.find(user => user.date === date).numOunces;
+    return dailyOunces;
   }
 
-  getDailyWaterIntake(idNumber, date) {
-    let theUser = this.data.find(
-      user => user.userID === idNumber && user.date === date
-    );
-    return theUser.numOunces;
-  }
-
-  getWeeklyWaterIntake(idNumber) {
-    let theUser = this.filterUser(idNumber);
-    let weekly = theUser.slice(theUser.length - 7, theUser.length + 1);
-
-    let weeklyWaterIntake = weekly.map(user => user.numOunces);
-
+  getWeeklyWaterIntake() {
+    let weekly = this.data.slice(this.data.length - 7, this.data.length + 1);
+    let weeklyWaterIntake = weekly.map(day => day.numOunces);
     return weeklyWaterIntake;
+  }
+
+  getAverageWaterConsumption() {
+    let totalWaterConsume = this.data.reduce(
+      (acc, day) => (acc += day.numOunces),
+      0
+    );
+    return parseInt(totalWaterConsume / this.data.length);
   }
 }
 
